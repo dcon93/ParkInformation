c49d0a2b42144eb27698adf38a83948d94e9ac54
Jared Smithers <SmithersJJ1@live.com>
2019-11-13 11:34:32 -0500

    added java bean for a daily forecast

---
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/DailyForecast.java
@@ -0,0 +1,49 @@
+package com.techelevator.npgeek;
+
+
+public class DailyForecast{
+	
+	String parkCode;
+	int day;
+	int tempLow; //Stored as Farenheit
+	int tempHigh; //Stored as Farenheit
+	String forecast;
+	
+	
+	
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public String getForecast() {
+		return forecast;
+	}
+	public void setForecast(String forecast) {
+		this.forecast = forecast;
+	}
+	public String getParkCode() {
+		return parkCode;
+	}
+	public void setParkCode(String parkCode) {
+		this.parkCode = parkCode;
+	}
+	public int getTempHigh() {
+		return tempHigh;
+	}
+	public void setTempHigh(int tempHigh) {
+		this.tempHigh = tempHigh;
+	}
+	public int getTempLow() {
+		return tempLow;
+	}
+	public void setTempLow(int tempLow) {
+		this.tempLow = tempLow;
+	}
+	
+	
+	
+	
+	
+}
