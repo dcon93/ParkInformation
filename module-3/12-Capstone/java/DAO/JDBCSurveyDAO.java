7708eeecd6d7b318db808d6871cee69e926cf1fb
Devin Conley <dcon89devin@gmail.com>
2019-11-13 14:09:14 -0500

    Added a save survey method to jdbcsurvey

--- module-3/12-Capstone/java/DAO/JDBCSurveyDAO.java
+++ module-3/12-Capstone/java/DAO/JDBCSurveyDAO.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
 import com.techelevator.npgeek.Survey;
 
-public class JDBCSurveyDAO {
 
+@Component
+public class JDBCSurveyDAO {
+private JdbcTemplate jdbcTemplate;
+	
+
+
+	@Autowired
+	public JdbcSurveyDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public void saveSurvey(Survey newSurvey) {
+		String sqlSaveSurvey = "INSERT INTO survey_result (parkcode, emailaddress, state, activitylevel) VALUES (?,?,?,?)";
+		jdbcTemplate.update(sqlSaveSurvey, newSurvey.getParkCode(), newSurvey.getEmail(), newSurvey.getState(), newSurvey.getActivityLevel());
+
+	}
 	
 }
