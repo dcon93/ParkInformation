6e38fae8f2407b7c7ffdb40be2edc0f6c9509be1
Devin Conley <dcon89devin@gmail.com>
2019-11-13 14:42:40 -0500

    Completed JDBCWeatherDAO

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBCWeatherDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBCWeatherDAO.java
@@ -1,5 +1,49 @@
 package com.techelevator.npgeek;
 
-public class JDBCWeatherDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JDBCWeatherDAO implements WeatherDAO {
+
+	
+	
+private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public JdbcWeatherDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	
+	@Override
+	public List<Weather> getWeatherByParkcode(String parkCode) {
+		
+		List<Weather> parkWeather = new ArrayList<>();
+		String sqlSelectParkWeather = "SELECT * FROM weather WHERE parkcode=?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSelectParkWeather, parkCode);
+		while(results.next()){
+			parkWeather.add(mapRowToWeather(results));
+		}
+		
+		return parkWeather;
+	}
+	
+	private Weather mapRowToWeather(SqlRowSet row){
+		Weather newWeather = new Weather();
+		newWeather.setFiveDayForcastValue(row.getInt("fivedayforecastvalue"));
+		newWeather.setForecast(row.getString("forecast"));
+		newWeather.setHigh(row.getInt("high"));
+		newWeather.setLow(row.getInt("low"));
+		return newWeather;
+	}
+	
+	
 
 }
