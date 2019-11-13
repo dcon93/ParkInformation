979edde820d2d6188092c8aafe1f71c32f0e7e7e
Devin Conley <dcon89devin@gmail.com>
2019-11-13 15:27:49 -0500

    Added another method for the .POST part of survey

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/SurveyController.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/SurveyController.java
@@ -1,29 +1,50 @@
 package com.techelevator.npgeek;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class SurveyController {
 
 	
 	@Autowired
-	SurveyDAO surveyDao;
+	SurveyDAO surveyDAO;
 	
 	@Autowired
-	ParkDAO parkDao;
+	ParkDAO parkDAO;
 	
-	
-	@RequestMapping(path="/placeholderForSurvey",method=RequestMethod.GET)
+	//Change what is in quotes once we are done with the jsp files
+	@RequestMapping(path="/placeholderForSurveyjsp",method=RequestMethod.GET)
 	public String showSurvey(ModelMap modelHolder) {
-		if (! modelHolder.containsAttribute("placeholderForSurvey")) {
+		if (! modelHolder.containsAttribute("placeholderForSurveyjsp")) {
 			modelHolder.put("survey", new Survey());
 		}
 
-	return "placeholderForSurvey";
+	return "placeholderForSurveyjsp";
 	
 	}
+	
+	@RequestMapping(path="/placeholderforsurvey.jsp", method=RequestMethod.POST)
+	public String processSurvey(@Valid @ModelAttribute Survey newSurvey, BindingResult result, RedirectAttributes flash) {
+		flash.addFlashAttribute("placeholderforsurveyjsp", newSurvey);
+		
+		if (result.hasErrors()) {
+			flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "placeholderforsurveyjsp", result);
+			return "redirect:/placeholderforsurveyjsp";
+		}
+		surveyDAO.saveSurvey(newSurvey);
+		
+		return "redirect:/placeholderforconfirmationjsp";
+	}
+	
+	
+	
 }
