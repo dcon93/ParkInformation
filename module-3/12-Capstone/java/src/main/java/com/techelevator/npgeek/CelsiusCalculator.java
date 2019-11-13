d5a8efdd13dfb84db049feb7726acfaa426793ff
Jared Smithers <SmithersJJ1@live.com>
2019-11-13 13:49:43 -0500

    updating calculator

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/CelsiusCalculator.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/CelsiusCalculator.java
@@ -3,8 +3,13 @@ package com.techelevator.npgeek;
 public class CelsiusCalculator {
 
 	
-	public static int farenheitToCelsius(int temp) {
-		double tempDouble = temp;
+	/*
+	 * @param temperature in farenheit
+	 *@return temperature in celsius
+	 *
+	 */
+	public static int farenheitToCelsius(int tempFarenheit) {
+		double tempDouble = tempFarenheit;
 		tempDouble = (tempDouble-32)*5/9;
 		return (int)Math.round(tempDouble);
 	}
