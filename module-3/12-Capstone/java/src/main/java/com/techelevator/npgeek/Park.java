d3a1c3614d6a417d6560442d6b6df41180290773
Jared Smithers <SmithersJJ1@live.com>
2019-11-13 11:10:03 -0500

    added Park Bean

---
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/Park.java
@@ -0,0 +1,113 @@
+package com.techelevator.npgeek;
+
+
+
+public class Park {
+	
+	
+	String parkCode;
+	String parkName;
+	String state;
+	int acreage;
+	int elevationInFeet;
+	float milesOfTrail;
+	int numberOfCampsites;
+	String climate;
+	int yearFounded;
+	int annualVisitorCount;
+	String inspirationalQuote;
+	String quoteSource;
+	String parkDescription;
+	
+	
+	public int getAcreage() {
+		return acreage;
+	}
+	public void setAcreage(int acreage) {
+		this.acreage = acreage;
+	}
+	public int getAnnualVisitorCount() {
+		return annualVisitorCount;
+	}
+	public void setAnnualVisitorCount(int annualVisitorCount) {
+		this.annualVisitorCount = annualVisitorCount;
+	}
+	public String getClimate() {
+		return climate;
+	}
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+	public int getElevationInFeet() {
+		return elevationInFeet;
+	}
+	public void setElevationInFeet(int elevationInFeet) {
+		this.elevationInFeet = elevationInFeet;
+	}
+	public String getInspirationalQuote() {
+		return inspirationalQuote;
+	}
+	public void setInspirationalQuote(String inspirationalQuote) {
+		this.inspirationalQuote = inspirationalQuote;
+	}
+	public float getMilesOfTrail() {
+		return milesOfTrail;
+	}
+	public void setMilesOfTrail(float milesOfTrail) {
+		this.milesOfTrail = milesOfTrail;
+	}
+	public int getNumberOfCampsites() {
+		return numberOfCampsites;
+	}
+	public void setNumberOfCampsites(int numberOfCampsites) {
+		this.numberOfCampsites = numberOfCampsites;
+	}
+	public String getParkCode() {
+		return parkCode;
+	}
+	public void setParkCode(String parkCode) {
+		this.parkCode = parkCode;
+	}
+	public String getParkDescription() {
+		return parkDescription;
+	}
+	public void setParkDescription(String parkDescription) {
+		this.parkDescription = parkDescription;
+	}
+	public String getParkName() {
+		return parkName;
+	}
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
+	}
+	public String getQuoteSource() {
+		return quoteSource;
+	}
+	public void setQuoteSource(String quoteSource) {
+		this.quoteSource = quoteSource;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getYearFounded() {
+		return yearFounded;
+	}
+	public void setYearFounded(int yearFounded) {
+		this.yearFounded = yearFounded;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
