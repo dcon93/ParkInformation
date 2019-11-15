0a3c2a0dd8ff833ad027c3386a6659ab83c2323e
Jared Smithers <SmithersJJ1@live.com>
2019-11-14 21:33:18 -0500

    last integration test written but not passing

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/DAO/WeatherDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/DAO/WeatherDAO.java
@@ -1,6 +1,8 @@
-package com.techelevator.npgeek;
+package com.techelevator.npgeek.DAO;
 
 import java.util.List;
+
+import com.techelevator.npgeek.Weather;
 
 public interface WeatherDAO {
 	public List<Weather> getWeatherByParkcode(String parkCode);
